d976f7b1cfbb81124c9c3ea4c60cf2b324aecca0
Dhya Innovations <dhyainnovations@Dhyas-Mac-mini.local>
2022-03-03 12:55:01 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,77 +1,29 @@
 import { Component } from '@angular/core';
-import { AnimationController, Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
-import { HttpService } from './shared/http.service';
-import Swal from 'sweetalert2';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public appPages = [
+    { title: 'Home', url: '/homepage',icon: 'assets/icon/home.PNG'},
+    { title: 'Set Alarm', url: '/setalarm', icon:'assets/icon/setalarm.png' },
+    { title: 'My Alarms', url: '/myalarms',  icon:'assets/icon/myalarms.png' },
+    { title: 'My Profile', url: '/myprofile', icon:'assets/icon/myprofile.png' },
+    { title: 'Support', url: '/support', icon:'assets/icon/support.png' },
+    // { title: 'Log out', url: '/signinpage', icon:'assets/icon/logout.png' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
+  constructor( private router: Router,) { }
 
-
-  constructor(private http: HttpService, private router: Router, route: ActivatedRoute, private platform: Platform,
-    private animaionCtrl: AnimationController) {
-
-    route.params.subscribe(val => {
-
-    });
-  }
-
-  ngOnInit() {
-    this.activeItem = "home"
-  }
   
-  
-
-  listOfProduct: any;
-  activeItem: any = "home";
-
-  close() {
-
-    this.router.navigate(['/'])
-  }
-
-  home() {
-    this.activeItem = "home"
-    this.router.navigate(['/homepage'])
-
-  }
-  setalarm() {
-    this.activeItem = "setalarm"
-    this.router.navigate(['/setalarm'])
-  }
-
-  myAlarm() {
-    this.activeItem = "myalarms"
-    this.router.navigate(['/myalarms'])
-  }
-
-
-  myprofile() {
-    this.activeItem = "myprofile"
-    this.router.navigate(['/myprofile'])
-  }
-
-  support() {
-    this.activeItem = "support"
-    this.router.navigate(['/support'])
-  }
 
   logout() {
-    this.activeItem = "home"
     localStorage.removeItem("24hrs-user-data")
     localStorage.removeItem("token")
-    this.router.navigate(['/'])
-    
-   
+    this.router.navigate(['/signinpage'])
   }
-
-
-
 
 }
