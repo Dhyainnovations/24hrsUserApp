7b5439d49ede7c08f35f5753e157e2dc1627dd3d
Dhya Innovations <dhyainnovations@Dhyas-Mac-mini.local>
2022-03-16 15:05:45 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,12 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
-import { LocalNotifications} from '@ionic-native/local-notifications/ngx'
+
 @NgModule({
   declarations: [AppComponent,],
   entryComponents: [],
   imports: [HttpClientModule,BrowserModule, IonicModule.forRoot(), AppRoutingModule,BrowserAnimationsModule,FormsModule],
-  providers: [LocalNotifications,Geolocation,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  providers: [Geolocation,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   
  ],
   bootstrap: [AppComponent],
